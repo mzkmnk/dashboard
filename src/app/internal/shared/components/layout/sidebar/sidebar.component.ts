437731be2f605d4ac1bdb2d2f7458bcd260a4339
mzkmnk <sma0729mex@gmail.com>
2024-07-20 00:24:27 +0900

    wip

--- src/app/internal/shared/components/layout/sidebar/sidebar.component.ts
+++ src/app/internal/shared/components/layout/sidebar/sidebar.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { PanelMenuModule } from 'primeng/panelmenu';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [PanelMenuModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
+  items: MenuItem[] = [];
 
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Travel Log',
+        items: [
+          {
+            label: 'Auth',
+            items: [
+              {
+                label: 'Sign In',
+              },
+              {
+                label: 'Sign Up',
+              },
+            ],
+          },
+          {
+            label: 'Internal',
+            items: [
+              {
+                label: 'Home',
+              },
+              {
+                label: 'Calendar',
+              },
+            ],
+          },
+        ],
+      },
+    ];
+  }
 }
