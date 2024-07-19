2eb7848e530ec54669caa8034eedfaaa0e37f7c6
sma0729mex <sma0729mex@gmail.com>
2024-07-19 13:55:08 +0000

    update:primeng,tailwind対応

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { SidebarComponent } from './internal/shared/components/layout/sidebar/sidebar.component';
 
 export const routes: Routes = [
     {
         path:'internal',
         loadChildren: () => import('./internal/internal.routes').then((M) => M.INTERNAL_ROUTES),
+        component:SidebarComponent
     },
     {
         path:'**',
