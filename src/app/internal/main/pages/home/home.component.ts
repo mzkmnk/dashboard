be1ff227204a4a579042373bc4ef8a82c07d455f
mzkmnk <sma0729mex@gmail.com>
2024-07-25 22:15:46 +0900

    wip

--- src/app/internal/main/pages/home/home.component.ts
+++ src/app/internal/main/pages/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component, inject, OnInit, Signal } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { sidebarSignalStore } from '../../../shared/store/sidebar/sidebar.signal-store';
-import { sampleTasks, Task } from '../../../data/task.data';
+import { Group } from '../../../data/task.data';
 import {
   GridsterComponent,
   GridsterConfig,
   GridsterItemComponent,
 } from 'angular-gridster2';
+import {
+  MainSignalStore,
+  ProjectSignalStoreModel,
+} from '../../store/main.signal-store';
 
 @Component({
   selector: 'app-home',
@@ -14,32 +18,44 @@ import {
   imports: [ButtonModule, GridsterComponent, GridsterItemComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
+  providers: [MainSignalStore],
 })
 export class HomeComponent implements OnInit {
   private readonly sidebarSignalStore = inject(sidebarSignalStore);
+  private readonly mainSignalStore = inject(MainSignalStore);
   $sidebarItem: Signal<string> = this.sidebarSignalStore.project.label;
+  $tasks: Signal<ProjectSignalStoreModel> = this.mainSignalStore.data;
+  groups: Group[] = ['TODO', 'IN PROGRESS', 'COMPLETED'];
 
   gridsterOptions: GridsterConfig = {};
 
-  taskData: Task[] = sampleTasks;
-
+  constructor() {
+    this.mainSignalStore.taskDataLoad();
+  }
   ngOnInit(): void {
     this.gridsterOptions = {
+      // ドラッグ設定
       draggable: {
-        // ドラッグ設定
-        enabled: true, // ドラッグ許可
+        // ドラッグ許可
+        enabled: true,
       },
+      // リサイズ設定
       resizable: {
-        // リサイズ設定
-        enabled: true, // リサイズを許可する
+        // リサイズを許可する
+        enabled: true,
       },
-      swap: true, // 入れ替えを許可する
-      minCols: 50, // 最小列数
-      maxCols: 50, // 最大列数(minCols以上はドラッグで表示される)
-      minRows: 50, // 最小行数
-      maxRows: 50, // 最大行数(minRows以上はドラッグで表示される)
+      // 入れ替えを許可する
+      swap: true,
+      // 最小列数
+      minCols: 50,
+      // 最大列数(minCols以上はドラッグで表示される)
+      maxCols: 50,
+      // 最小行数
+      minRows: 50,
+      // 最大行数(minRows以上はドラッグで表示される)
+      maxRows: 50,
       // 整列しない(自由)
-      // compactType: 'none',
+      compactType: 'none',
       // リサイズとドラッグでアイテムを押しのける
       pushItems: true,
       // 画面の幅が640px以下でグリッドを解除しアイテムを1列にする
