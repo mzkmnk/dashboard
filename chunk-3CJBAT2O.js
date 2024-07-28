8f59b66ecb193b74316f931ef8652253768c4c44
mzkmnk <mzkmnk@users.noreply.github.com>
2024-07-28 12:53:10 +0000

    deploy: 2a1c20bbb768a8bab7015ed9366594459307ce50

--- chunk-3CJBAT2O.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var e=[{path:"home",loadComponent:()=>import("./chunk-2GJXASAC.js").then(o=>o.HomeComponent)},{path:"**",redirectTo:"home"}];export{e as INTERNAL_ROUTES};
---
+++ chunk-RLKTNKJQ.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var e=[{path:"home",loadComponent:()=>import("./chunk-WV4WO2PR.js").then(o=>o.HomeComponent)},{path:"**",redirectTo:"home"}];export{e as INTERNAL_ROUTES};
