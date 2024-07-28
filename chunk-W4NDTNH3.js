f078b19c83c1d91922d89c2aec7adf20dac4ba9e
mzkmnk <mzkmnk@users.noreply.github.com>
2024-07-28 00:27:15 +0000

    deploy: 9008e890100eed8bb511903be3bace026c46951c

--- chunk-W4NDTNH3.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var e=[{path:"home",loadComponent:()=>import("./chunk-F6JR7D6V.js").then(o=>o.HomeComponent)},{path:"**",redirectTo:"home"}];export{e as INTERNAL_ROUTES};
