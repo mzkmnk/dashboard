e40abb8fcf45a6d4f3473c4354db348ada61f88b
mzkmnk <mzkmnk@users.noreply.github.com>
2024-07-28 11:16:25 +0000

    deploy: a838e34c12ed038d9ac03bb147c06742ab787337

---
+++ chunk-3CJBAT2O.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var e=[{path:"home",loadComponent:()=>import("./chunk-2GJXASAC.js").then(o=>o.HomeComponent)},{path:"**",redirectTo:"home"}];export{e as INTERNAL_ROUTES};
