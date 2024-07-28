c029d26d9d8a18191d783673520047ec583e7d61
mzkmnk <mzkmnk@users.noreply.github.com>
2024-07-28 01:42:37 +0000

    deploy: 1b154b6555a178b9d76b651c9eb62eb16a77c302

---
+++ chunk-GADE3UMA.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var e=[{path:"home",loadComponent:()=>import("./chunk-6QDFGFDF.js").then(o=>o.HomeComponent)},{path:"**",redirectTo:"home"}];export{e as INTERNAL_ROUTES};
