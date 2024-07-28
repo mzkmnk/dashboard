7a213acbda99723e4501c07861cdb0a036b20db5
mzkmnk <mzkmnk@users.noreply.github.com>
2024-07-28 23:50:59 +0000

    deploy: e5a5ee87b140f658026f1036550a9d0ccf8ea192

---
+++ chunk-D5M5GOCA.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var o=[{path:"auth",loadComponent:()=>import("./chunk-YZCOFY2I.js").then(t=>t.AuthComponent)},{path:"**",redirectTo:"auth"}];export{o as AUTH_ROUTES};
--- chunk-XPZSYLWZ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var o=[{path:"auth",loadComponent:()=>import("./chunk-FS7SVIAY.js").then(t=>t.AuthComponent)},{path:"**",redirectTo:"auth"}];export{o as AUTH_ROUTES};
