277de4609f8835667248231504d4d82a8e1cf735
mzkmnk <mzkmnk@users.noreply.github.com>
2024-07-29 22:44:35 +0000

    deploy: 2ab83eb60cac8a956ea441c784948ce60987fb5b

---
+++ chunk-UI6DEOJW.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var e=[{path:"home",loadComponent:()=>import("./chunk-COO32VHG.js").then(o=>o.HomeComponent)},{path:"**",redirectTo:"home"}];export{e as INTERNAL_ROUTES};
