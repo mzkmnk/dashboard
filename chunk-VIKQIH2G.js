f98e41ba4449cf795e4232c763dfec906730e288
mzkmnk <mzkmnk@users.noreply.github.com>
2024-07-30 13:34:54 +0000

    deploy: f53abdd9598fc3f79887e037d091de58228587d4

--- chunk-VIKQIH2G.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var e=[{path:"home",loadComponent:()=>import("./chunk-AA4C4VZ3.js").then(o=>o.HomeComponent)},{path:"**",redirectTo:"home"}];export{e as INTERNAL_ROUTES};
