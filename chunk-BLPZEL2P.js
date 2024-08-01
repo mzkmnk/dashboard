a655b4517ec2aed04b610f264008e1d32b915627
mzkmnk <mzkmnk@users.noreply.github.com>
2024-08-01 13:49:04 +0000

    deploy: c50290f39e9356cfd151b2b681f3e68b462219b6

---
+++ chunk-BLPZEL2P.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var e=[{path:"home",loadComponent:()=>import("./chunk-DDPXV5VZ.js").then(o=>o.HomeComponent)},{path:"**",redirectTo:"home"}];export{e as INTERNAL_ROUTES};
