19c94f813e212eb783c15bbb01b4d96871537a03
mzkmnk <mzkmnk@users.noreply.github.com>
2024-08-03 05:14:16 +0000

    deploy: b7682bb2cdcad4c6fd7f4a604a9a8107250120c4

--- chunk-S2DJBVB7.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var e=[{path:"home",loadComponent:()=>import("./chunk-J2NU6RT4.js").then(o=>o.HomeComponent)},{path:"**",redirectTo:"home"}];export{e as INTERNAL_ROUTES};
